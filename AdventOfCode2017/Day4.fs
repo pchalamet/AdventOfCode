e4130aa6585cfd0e660c1dbcaf9c19017a4683fb
Pierre Chalamet <pierre@chalamet.net>
2017-12-12 00:04:22 +0100

    day 4

---
+++ AdventOfCode2017/Day4.fs
@@ -0,0 +1,38 @@
+﻿module Day4
+open System.Linq
+open System
+
+
+let input2 = "aa bb cc dd aa"
+
+// --------------------------------------------------------------------------------------------------------------
+
+let reorderWords (line : string seq) =
+    let orderWord (word : string) =
+        word |> seq |> Seq.sort |> Seq.toArray |> System.String
+    let res = line |> Seq.map orderWord
+    res
+
+let do2() =
+    let lines = System.IO.File.ReadAllLines("data/Day4.txt")
+    let valid = lines |> Seq.fold (fun s t -> let items = t.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                                            |> reorderWords
+                                              s + (items.Distinct().Count() / items.Count())) 0
+    printfn "%d" valid
+
+
+
+// --------------------------------------------------------------------------------------------------------------
+
+let do1() =
+    let lines = System.IO.File.ReadAllLines("data/Day4.txt")
+    let valid = lines |> Seq.fold (fun s t -> let items = t.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                              s + (items.Distinct().Count() / items.Count())) 0
+    printfn "%d" valid
+
+
+// --------------------------------------------------------------------------------------------------------------
+
+let Do() =
+    //do1()
+    do2()
--- AdventOfCode2017/Program.fs
+++ AdventOfCode2017/Program.fs
@@ -4,6 +4,7 @@
 let main argv =
     //Day1.Do()
     //Day2.Do()
-    Day3.Do()
+    //Day3.Do()
+    Day4.Do()
 
     0 // return an integer exit code
