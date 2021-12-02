7d2b8f4776939fdf812243ad1b836e75bb1224db
Pierre Chalamet <pierre@chalamet.net>
2021-12-02 22:22:36 +0100

    code cleanup

--- 2021/fsharp/Day1.fsx
+++ 2021/fsharp/Day1.fsx
@@ -1,33 +1,25 @@
 open System.IO
 
 let readFile filename = 
-    filename |> File.ReadAllLines |> Seq.map int |> List.ofSeq
+    filename |> File.ReadAllLines |> Seq.map int
 
 
 let puzzle1() =
     let count = readFile "Input1.txt" 
-                |> List.pairwise 
-                |> List.filter (fun (x, y) -> x < y) 
-                |> List.length
+                |> Seq.pairwise 
+                |> Seq.filter (fun (x, y) -> x < y) 
+                |> Seq.length
 
     printfn $"Puzzle 1: {count}"
 
 
 let puzzle2() =
-    let values0 = readFile "Input1.txt"
-    let values1 = 0 :: values0 @ [ 0 ]
-    let values2 = 0 :: 0 :: values0
-    let values0 = values0 @ [ 0; 0 ]
-
-    let count = List.zip3 values2 values1 values0 
-                |> List.skip 2
-                |> List.rev
-                |> List.skip 2
-                |> List.rev
-                |> List.map (fun (x, y, z) -> x + y + z)
-                |> List.pairwise
-                |> List.filter (fun (x, y) -> x < y) 
-                |> List.length
+    let count = readFile "Input1.txt"
+                |> Seq.windowed 3
+                |> Seq.map (fun [| x; y; z |] -> x + y + z)
+                |> Seq.pairwise
+                |> Seq.filter (fun (x, y) -> x < y) 
+                |> Seq.length
 
     printfn $"Puzzle 2: {count}"
 
