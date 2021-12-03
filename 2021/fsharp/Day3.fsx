2754a8bfe5b01451aaace23bee036bbb4d2af298
Pierre Chalamet <pierre@chalamet.net>
2021-12-03 23:52:55 +0100

    move 2017

--- 2021/fsharp/Day3.fsx
+++ 2021/fsharp/Day3.fsx
@@ -4,13 +4,13 @@ open System.IO
 let readFile fileName = fileName |> File.ReadAllLines |> List.ofSeq
 
 let values = readFile "Input3.txt"
-let bitMax = values.[0].Length - 1
+let bitMax = values[0].Length - 1
 
 let computeGamma (values: string list) =
     let mutable gamma = ""
-    let bitMax = values.[0].Length - 1
+    let bitMax = values[0].Length - 1
     for idx in 0..bitMax do
-        let counts = values |> List.countBy (fun x -> x.[idx]) |> Map.ofList
+        let counts = values |> List.countBy (fun x -> x[idx]) |> Map.ofList
         let bitG = 
             match counts |> Map.tryFind '0', counts |> Map.tryFind '1' with
             | Some c0, Some c1 -> if c0 <= c1 then "1" else "0"
@@ -39,8 +39,8 @@ let puzzle2() =
         for idx in 0..bitMax do
             if 1 < res.Length then
                 let pattern: string = computePattern res
-                res <- res |> List.filter (fun x -> x.[idx] = pattern.[idx])
-        res.[0]
+                res <- res |> List.filter (fun x -> x[idx] = pattern[idx])
+        res[0]
 
     let oxygen = computeRate computeGamma |> bin2dec
     let co2 = computeRate (negate << computeGamma) |> bin2dec
