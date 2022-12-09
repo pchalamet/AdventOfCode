84eea5934d578a8558ad03d3a56d6d3d24935a14
Pierre Chalamet <pierre@chalamet.net>
2022-12-09 22:24:21 +0100

    day6

---
+++ 2022/day6.fsx
@@ -0,0 +1,25 @@
+#load "Helpers.fsx"
+open Helpers
+open System.Linq
+
+let inputfile = "inputs/day6.input"
+
+let score len =
+    let index =
+        inputfile
+        |> readlines
+        |> Seq.head
+        |> Seq.windowed len
+        |> Seq.findIndex (fun x -> x |> Array.distinct |> Array.length = len)
+    index + len
+
+let part1 () =
+    score 4
+    |> print1
+
+let part2 () =
+    score 14
+    |> print1
+
+part1()
+part2()
