8e8247a7998403f8fdc2feee625fed5b861ff4bd
Pierre Chalamet <pierre@chalamet.net>
2022-12-11 14:51:18 +0100

    fix performance day 11

--- 2022/day11.fsx
+++ 2022/day11.fsx
@@ -7,25 +7,22 @@ let inputfile = "inputs/day11.input"
 
 type MonkeyMath =
     | Square
-    | Multiply of BigInteger
-    | Add of BigInteger
+    | Multiply of int64
+    | Add of int64
 
 type Monkey =
-    { mutable Stats: BigInteger
-      Items: Queue<BigInteger>
+    { mutable Stats: int64
+      Items: Queue<int64>
       WorryCompute: MonkeyMath
-      Test: BigInteger
+      Test: int64
       TestTrue: int
       TestFalse: int }
-
-let toBigInteger s =
-    s |> int |> BigInteger
 
 let rec readMonkey lines =
     let items, lines = 
         match lines with
         | Regex "^  Starting items: (.+)$" [items] :: tail ->
-            items |> split ", " |> Seq.map toBigInteger |> Queue, tail
+            items |> split ", " |> Seq.map int64 |> Queue, tail
         | _ -> failwith "Invalid match"
 
     let maths, lines =
@@ -33,15 +30,15 @@ let rec readMonkey lines =
         | Regex "^  Operation: new = old \* old$" [] :: tail ->
             Square, tail
         | Regex "^  Operation: new = old \* (\d+)$" [by] :: tail ->
-            Multiply (toBigInteger by), tail
+            Multiply (int64 by), tail
         | Regex "^  Operation: new = old \+ (\d+)$" [by] :: tail ->
-            Add (toBigInteger by), tail
+            Add (int64 by), tail
         | _ -> failwith $"Invalid match {lines}"
 
     let tests, lines =
         match lines with
         | Regex "^  Test: divisible by (\d+)$" [by] :: tail ->
-            toBigInteger by, tail
+            int64 by, tail
         | _ -> failwith "Invalid match"
 
     let testTrue, lines =
@@ -56,7 +53,7 @@ let rec readMonkey lines =
             int target, tail
         | _ -> failwith "Invalid match"
 
-    { Stats = BigInteger.Zero
+    { Stats = 0L
       Items = items
       WorryCompute = maths
       Test = tests
@@ -75,7 +72,7 @@ let rec readMonkeys monkeys lines =
 let round (monkeys: Monkey list) boredom (roundNumber: int) =
     for monkey in monkeys do
         while 0 < monkey.Items.Count do
-            monkey.Stats <- monkey.Stats + BigInteger.One
+            monkey.Stats <- monkey.Stats + 1L
             let item = monkey.Items.Dequeue()
             let item =
                 match monkey.WorryCompute with
@@ -83,24 +80,26 @@ let round (monkeys: Monkey list) boredom (roundNumber: int) =
                 | Multiply by -> item * by
                 | Add value -> item + value
             let item = boredom item
-            let target = (item % monkey.Test = BigInteger.Zero) ? (monkey.TestTrue, monkey.TestFalse)
+            let target = (item % monkey.Test = 0L) ? (monkey.TestTrue, monkey.TestFalse)
             // printfn $"round {roundNumber}: monkey sends to monkey {target} item {item}"
             monkeys[target].Items.Enqueue(item)
 
-    printfn $"*** round {roundNumber}"
+    // printfn $"*** round {roundNumber}"
     // monkeys |> List.iteri (fun idx monkey ->
     //                         printf $"Monkey {idx} ({monkey.Stats}):"
     //                         for item in monkey.Items do
     //                              printf $" {item}"
     //                         printfn "")
 
-let score boredom maxrounds =
+let parse () =
     let lines =
         inputfile
         |> readlines
         |> List.ofArray
 
-    let monkeys = readMonkeys [] lines
+    readMonkeys [] lines
+
+let run boredom maxrounds monkeys =
     for roundNumber in 1..maxrounds do
         round monkeys boredom roundNumber
 
@@ -108,20 +107,24 @@ let score boredom maxrounds =
     |> List.sortByDescending (fun m -> m.Stats)
     |> List.map (fun m -> m.Stats)
     |> List.take 2
-    |> List.fold (*) BigInteger.One
+    |> List.fold (*) 1L
 
 let part1() =
-    let boredom (item: BigInteger) = item / BigInteger(3)
+    let boredom item = item / 3L
 
-    score boredom 20
+    parse()
+    |> run boredom 20
     |> print1
 
 let part2() =
-    let boredom item = item
+    let monkeys = parse()
+    let m = monkeys |> List.fold (fun acc m -> acc * m.Test) 1L
 
-    score boredom 10000
+    let boredom item = item % m
+
+    monkeys    
+    |> run boredom 10000
     |> print2
-
 
 part1()
 part2()
